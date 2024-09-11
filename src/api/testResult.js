3ba63c0404cf283ad581ab9460334c42e52209e9
지영 <rooftop7788@naver.com>
2024-09-11 17:03:43 +0900

    fix: json-server url change

--- src/api/testResult.js
+++ src/api/testResult.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/testResults";
+const API_URL = "https://fuschia-bloom-cost.glitch.me/testResults";
 
 export const getTestResults = async () => {
   const response = await axios.get(API_URL);
